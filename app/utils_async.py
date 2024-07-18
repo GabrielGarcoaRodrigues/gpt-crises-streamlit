d496cf763fb5522864611b25cd883b8eee4dd568
Gabriel <60533817+GabrielGarcoaRodrigues@users.noreply.github.com>
2024-07-18 00:32:25 -0300

    att

--- app/utils_async.py
+++ app/utils_async.py
@@ -72,10 +72,10 @@ async def retorna_valor_final(results):
     
     prompt.append({'role': 'system',  'content' : prompt_final})
     
-    # for i in results:
-    #     texto_concatenado = texto_concatenado + " \n "+i
+    for i in results:
+        texto_concatenado = texto_concatenado + " \n "+i
     
-    # prompt.append({'role': 'user', 'content':f"lista de análises: {texto_concatenado}"})
+    prompt.append({'role': 'user', 'content':f"lista de análises: {texto_concatenado}"})
     resultado_final = await make_api_call_to_gpt(prompt)
     
     print(f"##### Resultado final...{datetime.datetime.now()}: {resultado_final}")
@@ -95,16 +95,25 @@ async def process_comments(df, context):
     for i in concatenados:
         prompts = []
         prompts.append({'role': 'system',  'content' : description})    
-        prompts.append({'role': 'system',  'content' : f"O contexto da análise é:{context}"})    
+        prompts.append({'role': 'system',  'content' : f"O contexto da análise é:{context}"}) 
+        prompts.append({'role': 'system',  'content' : '''
+        Analise TODOS os comentários do contexto e faça as seguintes tarefas:
+        1. Classifique os sentimentos de todos os comentários, mostrando no resultado final o percentual e número absoluto de cada sentimento em relação ao total;
+        2. Leia os comentários, e a partir da leitura de todos eles, crie 5 categorias em formato de uma frase curta para cada categoria, mostrando também a quantidade de comentários relacionados a cada categoria  além de uma pequena lista com algumas palavras chave relacionadas a categoria. Além disso Para cada categoria criada, gere um comentário curto que esteja no mesmo modelo dos comentários analisados e que sintetize a maior parte dos comentários relacionados a categoria. Gere uma breve descrição de cada categoria baseado nos comentários; 
+        3. Faça um breve resumos dos comentários que não estão relacionados a nenhuma das categorias.
+        4. Faça uma breve análise dos comentários positivos, neutros e negativos;
+        5. Faça uma análise única juntando quantitaiva e qualitativa dos comentários.
+        '''})    
         prompts.append({'role': 'user',  'content' : f"comentários: {i}"})
 
         dicionario_de_prompts.append(prompts)
         
-    # results = []
-    # tasks = [make_api_call_to_gpt(prompt) for prompt in dicionario_de_prompts]
-    # results = await asyncio.gather(*tasks)
-    # st.write(results)
-    # print("Gerando resultado final...")
+    st.write(dicionario_de_prompts)
+
+    results = []
+    tasks = [make_api_call_to_gpt(prompt) for prompt in dicionario_de_prompts]
+    results = await asyncio.gather(*tasks)
+    print("Gerando resultado final...")
     resultado_final = await retorna_valor_final(dicionario_de_prompts)
 
     return resultado_final
