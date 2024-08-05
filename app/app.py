98686cc25f7f084c0c3056445e636e274867256a
Gabriel <60533817+GabrielGarcoaRodrigues@users.noreply.github.com>
2024-08-05 14:18:50 -0300

    att

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import asyncio
-from utils_async import *
+from utils_async import process_comments
 from datetime import datetime
 from utils_files import *
 import time
@@ -9,7 +9,9 @@ import emoji
 import re
 
 
+# Funções Assíncronas
 def get_event_loop():
+    """Garante que um event loop esteja disponível, criando um se necessário."""
     try:
         return asyncio.get_event_loop()
     except RuntimeError as e:
@@ -19,41 +21,99 @@ def get_event_loop():
             return loop
         raise
 
+async def async_process_comments(df, context):
+    """Processa comentários de forma assíncrona."""
+    return await process_comments(df, context)
 
 async def run_async_process(df, context, progress_bar):
     loop = get_event_loop()
     task = loop.create_task(process_comments(df, context))
-
+    
     progress = 0
     while not task.done():
-        await asyncio.sleep(6)
+        await asyncio.sleep(11)  # Aguarda 5 segundos entre cada atualização
         progress += 10
         progress_bar.progress(min(progress, 100))
-
+    
     return await task
+
+# Função Principal
+def main():
+    st.set_page_config(page_title="Orbit AI", layout='centered')
+    print(f"##### Executando Main...{datetime.now()}")
+
+    inicializacao() 
+
+    st.header('Ambev Crises 📈')
+    if st.button('Melhores Práticas'):
+        st.session_state.show_info = not st.session_state.get('show_info', False)
+    if st.session_state.get('show_info', False):
+            with st.expander("Como utilizar o chat"):
+                st.write("""
+                    1. Faça o upload de um arquivo Excel com os comentários sobre a crise (O arquivo deve conter uma coluna chamada 'Texto').
+                    2. Descreva o contexto da análise no campo de texto (Marca e Causa).
+                    3. Clique no botão ou pressione `Enter` para processar a análise.
+                    4. Aguarde enquanto o processamento é realizado. O resultado será exibido na tela.
+                """)
+            with st.expander("Como escrever o contexto"):
+                st.write("""
+                    O contexto é uma descrição curta da crise ou evento que gerou os comentários, e deve conter a marca e causa da crise. 
+                    Por exemplo, se os comentários são sobre uma mudança de ingredientes na cerveja Heineken, 
+                    o contexto poderia ser: "A mudança nos ingredientes da cerveja Heineken".
+                """)
+            with st.expander("Comentários removidos"):
+                st.write("""
+                    O modelo remove os comentários que são linhas vazias e menções a outros usuários, para garantir uma análise precisa sobre o tema.
+                """)
+            st.info("O modelo analisa no máximo 1000 comentários por vez.")
+     
+    uploaded_file = st.file_uploader("Faça o upload do arquivo Excel com os comentários sobre a crise", type="xlsx", help="O arquivo deve conter uma coluna chamada 'Texto'.", accept_multiple_files=False)
+    df_texto = handle_uploaded_file(uploaded_file)
+    
+    prompt = st.chat_input(placeholder='Descreva o contexto da análise...', max_chars=250, key='input')    
+
+    if prompt:
+        if df_texto is None:
+            st.error("Faça o upload do arquivo Excel!")
+        else:
+            st.session_state.processing = True
+            api_key = st.session_state['api_key']
+            st.write("Processando...")
+            # Mostrar barra de progresso
+            progress_bar = st.progress(0)
+            
+            # Executa o processamento assíncrono e atualiza a barra de progresso
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            results = loop.run_until_complete(run_async_process(df_texto, prompt, progress_bar))
+            
+            st.session_state.processing = False
+            display_results(results)
 
 
 def clean_text(text):
-    text = re.sub(r'@\w+', '', text)
-    text = text.strip()
+    """Remove emojis e menções de usuários do texto."""
+    text = re.sub(r'@\w+', '', text)  # Remove menções de usuários
+    text = text.strip()  # Remove espaços em branco extras
     return text
 
 
 def handle_uploaded_file(uploaded_file, limit=5000):
+    """Processa o arquivo carregado e retorna um DataFrame se encontrado. limit é o tamanho maximo de textos"""
     try:
         if uploaded_file:
             df = pd.read_excel(uploaded_file)
             tamanho = len(df)
-            df = df[df['Texto'].apply(lambda x: isinstance(x, str))]
-            df['Texto'] = df['Texto'].apply(clean_text)
-            df = df.dropna(subset=['Texto'])
-            df = df[df['Texto'] != '']
+            df = df[df['Texto'].apply(lambda x: isinstance(x, str))]  # Filtra apenas strings
+            df['Texto'] = df['Texto'].apply(clean_text)  # Limpa o texto
+            df = df.dropna(subset=['Texto'])  # Remove linhas em branco
+            df = df[df['Texto'] != '']  # Remove linhas que ficaram vazias após a limpeza
             df = df.head(limit)
-            df.reset_index(drop=True, inplace=True)
+            df.reset_index(drop=True, inplace=True)  # Redefine o índice do DataFrame
             if 'Texto' in df.columns:
-                st.dataframe(df['Texto'])
+                st.dataframe(df['Texto'])  # Mostra o dataframe
                 tamanho_limpo = len(df)
-                st.info(f"{tamanho - tamanho_limpo} comentários foram removidos por não contribuírem com a análise!")
+                st.info(f"{tamanho - tamanho_limpo} comentários foram removidos.")
                 return df
             else:
                 st.error("A coluna 'Texto' não foi encontrada no arquivo.")
@@ -61,71 +121,22 @@ def handle_uploaded_file(uploaded_file, limit=5000):
         st.error("Erro: A coluna 'Texto' não foi encontrada no arquivo.")
     return None
 
-
 def display_results(results):
+    """Exibe os resultados processados."""
     if results:
         results_str = ''.join(results)
         st.write(results_str)
 
-
 def inicializacao():
+    """Inicializa a sessão do Streamlit, se necessário."""
     if 'modelo' not in st.session_state:
         st.session_state.modelo = 'gpt-4o'
     if 'api_key' not in st.session_state:
-        st.session_state.api_key = st.secrets["API_KEY"]
+        st.session_state.api_key = le_chave()
     if 'show_info' not in st.session_state:
         st.session_state.show_info = False
-
-
-def main():
-    st.set_page_config(page_title="GPT Crises | Ambev", layout='centered')
-    print(f"##### Executando Main...{datetime.now()}")
-
-    inicializacao()
-
-    st.header('Ambev Crises 📈')
-    if st.button('Melhores Práticas'):
-        st.session_state.show_info = not st.session_state.get('show_info', False)
-    if st.session_state.get('show_info', False):
-        with st.expander("Como utilizar o chat"):
-            st.write("""
-                1. Faça o upload de um arquivo Excel com os comentários sobre a crise (O arquivo deve conter uma coluna chamada 'Texto').
-                2. Descreva o contexto da análise no campo de texto (Marca e Causa).
-                3. Clique no botão ou pressione `Enter` para processar a análise.
-                4. Aguarde enquanto o processamento é realizado. O resultado será exibido na tela.
-            """)
-        with st.expander("Como escrever o contexto"):
-            st.write("""
-                O contexto é uma descrição curta da crise ou evento que gerou os comentários, e deve conter a marca e causa da crise.
-                Por exemplo, se os comentários são sobre uma mudança de ingredientes na cerveja Heineken,
-                o contexto poderia ser: "A mudança nos ingredientes da cerveja Heineken".
-            """)
-        with st.expander("Comentários removidos"):
-            st.write("""
-                O modelo remove os comentários que são linhas vazias e menções a outros usuários, para garantir uma análise precisa sobre o tema.
-            """)
-        st.info("O modelo analisa no máximo 1000 comentários por vez.")
-
-    uploaded_file = st.file_uploader("Faça o upload do arquivo Excel com os comentários sobre a crise", type="xlsx", help="O arquivo deve conter uma coluna chamada 'Texto'.", accept_multiple_files=False)
-    df_texto = handle_uploaded_file(uploaded_file)
-
-    prompt = st.chat_input(placeholder='Descreva o contexto da análise...', max_chars=250, key='input')
-
-    if prompt:
-        if df_texto is None:
-            st.error("Faça o upload do arquivo Excel!")
-        else:
-            st.session_state.processing = True
-            st.write("Processando...")
-            progress_bar = st.progress(0)
-
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            results = loop.run_until_complete(run_async_process(df_texto, prompt, progress_bar))
-
-            st.session_state.processing = False
-            display_results(results)
-
+    
+    print("##### Sessão inicializada.")
 
 if __name__ == '__main__':
     main()
--- app/utils_async.py
+++ app/utils_async.py
@@ -7,43 +7,50 @@ from utils_files import *
 
 
 headers = {
-    'Authorization': f'Bearer {st.secrets["API_KEY"]}',
-    'Content-Type': 'application/json'
+    'Authorization': f'Bearer {le_chave()}',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization'
 }
+
 
 description = """
 Você é um analista de crises de marcas e trabalha para a marca Ambev. 
 O usuário irá inserir uma lista de comentários de redes sociais.
-Seu trabalho é ler todos os comentários e fazer as análises solicitadas a partir do contexto dado."""
+Seu trabalho é ler todos os comentários e fazer as análises solicitadas a partir do contexto dado.
+"""
 
 prompt_final = '''
 Analise TODOS os comentários do contexto e faça as seguintes tarefas:
 1. Classifique os sentimentos de todos os comentários, mostrando no resultado final o percentual e número absoluto de cada sentimento em relação ao total;
-2. Crie 5 categorias em formato de uma frase curta a partir de todas as categrias criadas, mostrando também a quantidade(número absoluto e porcentagem) de comentários relacionados a cada categoria  além de uma pequena lista com algumas palavras chave relacionadas a categoria. Além disso Para cada categoria criada, gere um comentário curto que esteja no mesmo modelo dos comentários analisados e que sintetize a maior parte dos comentários relacionados a categoria. Gere uma breve descrição de cada categoria baseado nos comentários, e calcule a porcentagem dos sentimentos, positivos, negativos e neutros de cada categoria.; 
-3. Faça um breve resumos dos comentários que não estão relacionados a nenhuma das 5 categorias.
+2. Crie 5 categorias em frases curtas a partir das categorias existentes, mostrando a quantidade (número e porcentagem) de comentários para cada categoria (Não precisa englobar todos os comentários nessas categorias), além de palavras-chave relacionadas; Para cada categoria, gere um comentário curto que sintetize a maioria dos comentários; Forneça uma breve descrição de cada categoria baseada nos comentários; Calcule a porcentagem de sentimentos positivos, negativos e neutros de cada categoria.
+3. Faça um breve resumo dos comentários que não estão relacionados a nenhuma das 5 categorias.
 4. Faça uma breve análise dos comentários positivos, neutros e negativos;
 5. Faça uma análise que relacione a quantitativa com a qualitativa dos comentários.
 '''
 
-def dividir_dataframe_em_blocos(df, tamanho_bloco=200):   
+def dividir_dataframe_em_blocos(df, tamanho_bloco=200):
     if 'Texto' not in df.columns:
-       raise ValueError("A coluna 'Texto' não está presente no DataFrame.")
-    
+        raise ValueError("A coluna 'Texto' não está presente no DataFrame.")
+
     num_blocos = (len(df) + tamanho_bloco - 1) // tamanho_bloco
     lista_de_textos_bloco = [df['Texto'][i*tamanho_bloco:(i+1)*tamanho_bloco].tolist() for i in range(num_blocos)]
-    return lista_de_textos_bloco
     
+    return lista_de_textos_bloco
+
 def concatena_textos_blocos(blocos_de_textos):    
     lista_de_strings = []
     for bloco in blocos_de_textos:
         # Concatenar os textos do bloco com quebra de linha entre eles
         texto_concatenado = '\n'.join(bloco)
         lista_de_strings.append(texto_concatenado)
-        
+    
     return lista_de_strings
 
 async def make_api_call_to_gpt(prompt):
-    # st.write(prompt) isso aqui ta estranho
+    print(f"##### Calling API...: {datetime.datetime.now()}")
+    # print(prompt)
     async with aiohttp.ClientSession() as session:                
         payload = {
             "model": "gpt-4o",
@@ -80,10 +87,8 @@ async def retorna_valor_final(results):
     
     print(f"##### Resultado final...{datetime.datetime.now()}: {resultado_final}")
     
-    return resultado_final
- 
-
-
+    return resultado_final   
+    
 async def process_comments(df, context):
     
     print(f"##### Async Process Init...{datetime.datetime.now()}")
@@ -98,19 +103,18 @@ async def process_comments(df, context):
         prompts.append({'role': 'system',  'content' : description})    
         prompts.append({'role': 'system',  'content' : f"O contexto da análise é:{context}"})    
         prompts.append({'role': 'user',  'content' : f"comentários: {i}"})
-        # ISSO AQUI TEM QUE RECEBER O PROMPT FINAL
         dicionario_de_prompts.append(prompts)
-        
+    
+    print("*************DICIONARIO")
+    print(dicionario_de_prompts[0])
     results = []
     tasks = [make_api_call_to_gpt(prompt) for prompt in dicionario_de_prompts]
     results = await asyncio.gather(*tasks)
-    # st.write(results)
+    
     print("Gerando resultado final...")
     resultado_final = await retorna_valor_final(dicionario_de_prompts)
-
+    
     return resultado_final
-
-
 
 if __name__ == "__main__":
     asyncio.run(process_comments())
